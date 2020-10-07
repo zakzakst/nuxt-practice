336568bfe2648b04121c57e6ad0effb287cf05d9
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-10-08 08:52:51 +0900

    おみくじページ作成

---
+++ pages/finish/vue-i18n.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>{{ $t('title') }}</p>
+    <button class="button" @click="toggleLocale">切り替え</button>
+  </div>
+</template>
+
+<script>
+export default {
+   asyncData({ app }) {
+    const locale = app.$cookies.get('locale') || 'ja';
+    return {
+      isJa: locale === 'ja'
+    }
+  },
+  methods: {
+    toggleLocale() {
+      this.isJa = !this.isJa;
+      const locale = this.isJa ? 'ja' : 'en';
+      // localeをCookieに保存
+      this.$cookies.set('locale', locale, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 365
+      });
+      // 言語を切り替え
+      this.$i18n.locale = locale;
+    }
+  }
+}
+</script>
