a74589de1dedf2649a4652b9e6a83efdeb1197aa
satoshi ishizaki <e1433104@yahoo.co.jp>
2020-10-10 16:24:51 +0900

    アコーディオンページの作成

---
+++ pages/finish/accordion.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="content">
+    <h1>アコーディオン</h1>
+    <accordion>
+      <div slot="title">Chapter1-1</div>
+      <div slot="detail">
+        <h2>UI / UX</h2>
+        <p>ページ遷移が必要ない。リアルタイムでのフィードバックが必要なサイトが増えてきた</p>
+      </div>
+    </accordion>
+    <accordion>
+      <div slot="title">Chapter1-2</div>
+      <div slot="detail">
+        <h2>jQueryとの違い</h2>
+        <p>jQueryはDOMを外部から触るライブラリ</p>
+        <p>Vue.jsはデータ（ViewModel）を管理することで仮想DOMを描画するライブラリ</p>
+      </div>
+    </accordion>
+    <accordion>
+      <div slot="title">Chapter1-3</div>
+      <div slot="detail">
+        <h2>React.jsとの違い</h2>
+        <p>データによって仮想DOMを描画する点は一緒</p>
+        <p>HTMLをそのままテンプレートとして利用できる</p>
+        <p>つまりコンパイルも不要</p>
+      </div>
+    </accordion>
+  </div>
+</template>
+
+<script>
+import Accordion from '~/components/presentational/Accordion';
+
+export default {
+  components: {
+    Accordion,
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
